201694d6f0e306378bd15ac667f89d97d5c84876
QwertyAswal <aswals8991@gmail.com>
2021-04-14 13:28:27 +0530

    Added front end from web server project

---
+++ src/app.js
@@ -0,0 +1,79 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+
+const app = express()
+
+
+// Path for express config
+const publicPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+// Setup static directory
+app.use(express.static(publicPath))
+
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Shubham Aswal'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Developer',
+        name: 'Shubham Aswal'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'Some informative help text!!',
+        title: 'Help',
+        name: 'Shubham Aswal'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        res.send({
+            error: 'address feild must be provided'
+        })
+    }
+    else {
+        res.send({
+            address: req.query.address
+        })
+    }
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        errorMsg: 'Help article not found!!',
+        title: '404',
+        name: 'Shubham Aswal'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        errorMsg: 'Page not found!!',
+        title: '404',
+        name: 'Shubham Aswal'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Listening at port 3000.')
+})
